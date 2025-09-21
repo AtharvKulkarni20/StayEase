78fbe35152f6c18566e4fed056951e4c9387b001
Atharv <atharvnandkishorkulkarni@gmail.com>
2025-09-21 15:01:28 +0530

    Diving into middlewares

--- middlewares/demo.js
+++ middlewares/demo.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ExpressError = require("./ExpressError");
 
+const CheckToken = (req, res, next) => {
+  let { token } = req.query;
+  if (token === "ANK") {
+    next();
+  }
+  throw new ExpressError( 401, "ACCESS DENIED");
+};
 
-app.use((req,res,next)=>{
-    const {token} = req.query
-    if(token==="ANK"){
-        next();
-    }else{
-        res.status(401).send("Unauthorized")
-    }
-    
+app.get("/admin", (req,res)=>{
+    throw new ExpressError(403, "No Admin Found");
+})
+app.use((err,req,res,next)=>{
+    let {status=500, message} = err;
+    res.status(status).send(message);
 })
 
-app.get("/", (req, res) => {
-    res.send("Route Working");
-})
+app.get("/profile", (req, res) => {
+  data = data;
+});
+
+app.get("/", CheckToken, (req, res) => {
+  res.send("Route Working");
+});
 
 app.listen(8080, () => {
-    console.log("Server is Listning at PORT 8080 📡");
-})
+  console.log("Server is Listning at PORT 8080 📡");
+});
